3c508ff13a7069e96ee6395cb219b0e42abe1e5b
Jason Snider <root@jasonsnider.com>
2013-07-31 05:46:10 -0700

    Improved naming conventions, changed lightbox to ajax-light box

--- ajax-lightbox.js
+++ ajax-lightbox.js
@@ -1,5 +1,5 @@
 /**
- * LightBox - lightbox.js
+ * AjaxLightBox - AjaxLightBox.js
  * 
  * Copyright 2013, Jason D Snider. (http://jasonsnider.com)
  * Licensed under The MIT License
@@ -13,36 +13,36 @@
     $(function () {
         
         /**
-         * Creates a lightbox and injects the into it the targeted Ajax request
+         * Creates a AjaxLightBox and injects the into it the targeted Ajax request
          *
-         * data-toggle = "lightbox" - Creates a trigger for opening a lightbox
+         * data-toggle = "AjaxLightBox" - Creates a trigger for opening a AjaxLightBox
          * data-url = "the path to an Ajax request"
          */
-        $(document).on('click.data-api', '[data-toggle="lightbox"]', function (e) {
+        $(document).on('click.data-api', '[data-toggle="ajax-lightbox"]', function (e) {
                 
             e.preventDefault();
             
             var $this = $(this),
                 $url = $this.attr('data-url');
 
-            if ($('#LightBox').length == 0) {
+            if ($('#AjaxLightBox').length == 0) {
                     
                 /* Add the lighbox to the DOM */
                 $('body').prepend(
-                    '<div id="LightBox">'
-                        + '<div id="LightBoxContent">'
-                            + '<div id="LightBoxHeader" class=\"clearfix\">'
-                                + '<a href="#" data-toggle="lightbox-close" class="lightbox-close">&times;</a>'
+                    '<div id="AjaxLightBox">'
+                        + '<div id="AjaxLightBoxContent">'
+                            + '<div id="AjaxLightBoxHeader" class=\"clearfix\">'
+                                + '<a href="#" data-toggle="ajax-lightbox-close" class="ajax-lightbox-close">&times;</a>'
                             + '</div>'
-                            + '<div id="LightBoxData"></div>'
-                            + '<div id="LightBoxFooter" class=\"clearfix\">'
-                                + '<a href="#" data-toggle="lightbox-close" class="lightbox-close-button">[Close]</a>'
+                            + '<div id="AjaxLightBoxData"></div>'
+                            + '<div id="AjaxLightBoxFooter" class=\"clearfix\">'
+                                + '<a href="#" data-toggle="ajax-lightbox-close" class="ajax-lightbox-close-button">[Close]</a>'
                             + '</div>'
                         + '</div>'
                     + '</div>'    
                 );
 
-                $('#LightBoxData').load($url);
+                $('#AjaxLightBoxData').load($url);
                  
                 /* Remove the default scroll bar, we want the light box to take control */
                 $('body').attr('style', 'overflow:hidden');	
@@ -52,25 +52,25 @@
         });
             
         /**
-         * Closes the lightbox
+         * Closes the AjaxLightBox
          * 
-         * data-toggle = "lightbox-close" - Creates a trigger for closing a lightbox
+         * data-toggle = "AjaxLightBox-close" - Creates a trigger for closing a AjaxLightBox
          */
-        $(document).on('click.data-api', '[data-toggle="lightbox-close"]', function (e) {
+        $(document).on('click.data-api', '[data-toggle="ajax-lightbox-close"]', function (e) {
                 
             e.preventDefault();
                     
-            if ($('#LightBox').length > 0) {
+            if ($('#AjaxLightBox').length > 0) {
                     
-                /* Remove the lightbox from the DOM */
-                $('#LightBox').remove();
+                /* Remove the AjaxLightBox from the DOM */
+                $('#AjaxLightBox').remove();
                 /* Return the default scroll bar*/
                 $('body').attr('style', 'overflow:auto');	
                
             }
                 
         });
-            
+
     });
         
 }(jQuery));
